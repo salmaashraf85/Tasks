6431daeee5274daa07bf2de3539e0494a33f1212
Salma Ashraf <loma852004@gmail.com>
2025-02-17 23:57:50 +0200

    Problem F is Added

---
+++ day1/Problem F.cs
@@ -0,0 +1,11 @@
+﻿using System;
+class Program
+{
+    static void Main()
+    {
+        string[] input = Console.ReadLine().Split(" ");
+        long x = long.Parse(input[0]);
+        long y = long.Parse(input[1]);
+        Console.WriteLine((x % 10) + (y % 10));
+    }
+}
