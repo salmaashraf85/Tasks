28019832df99b62482059ddd4f5e1175ec8c1f03
Salma Ashraf <loma852004@gmail.com>
2025-02-17 15:37:46 +0200

    problems solutions

---
+++ day1/Problem A-.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        string Name;
+        Name = Console.ReadLine();
+        Console.WriteLine($"Hello, {Name}\n");
+    }
+}
