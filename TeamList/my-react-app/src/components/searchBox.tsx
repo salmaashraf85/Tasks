e5e43e009e47d1e961665f31b4e6fe29d80f8384
Salma Ashraf <loma852004@gmail.com>
2025-05-05 12:40:30 +0300

    last commit

--- TeamList/my-react-app/src/components/searchBox.tsx
+++ TeamList/my-react-app/src/components/searchBox.tsx
@@ -16,7 +16,7 @@ const SearchInputBox = () => {
         ),
       }}
       sx={{
-        width: 300, 
+        width: 250, 
         '& .MuiOutlinedInput-root': {
           borderRadius: '20px', 
           backgroundColor: '#f5f5f5', 
