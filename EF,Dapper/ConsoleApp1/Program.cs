0e14bbd2cdcdfd1ee6a31119a5874e6ac699cace
Salma Ashraf <loma852004@gmail.com>
2025-08-24 00:38:27 +0300

    [FIX]:EF task

--- EF,Dapper/ConsoleApp1/Program.cs
+++ EF,Dapper/ConsoleApp1/Program.cs
@@ -8,24 +8,122 @@ namespace ConsoleApp1
     {
         static void Main(string[] args)
         {
-          
+            //add category
             using (var context = new AppDBcontext())
             {
-                var repo = new CategoryRepository(context);
+                var category = new Category
+                {
+                    Name="Toys"
+                };
 
-                //// CREATE
-                //Category category = new Category();
-                //category.Name = "Snacks";
-                //repo.Add(category);
-                //// READ
-                //var category1 = repo.GetById(1);
-                //Console.WriteLine(category1.Name);
-
-         
-                // DELETE
-                repo.DeleteById(1);
-                   
+                bool exists = context.Categories.Any(c => c.Name == category.Name);
+                if (!exists)
+                {
+                    context.Categories.Add(category);
+                    context.SaveChanges();
+                }
+                else
+                {
+                    Console.WriteLine("this is already exists");
+                }
             }
+
+            //add product
+
+            using (var context = new AppDBcontext())
+            {
+                var product = new Product
+                {
+                    Name = "yoyo",
+                    Price=100,
+                    CategoryId=1
+                };
+
+                  context.Products.Add(product);
+                  context.SaveChanges();
+              
+            }
+
+            //update product 
+            using (var context = new AppDBcontext())
+            {
+                Product product= context.Products.FirstOrDefault(p => p.Id == 1);
+
+                if (product != null)
+                {
+                    product.Price= 100;
+                    context.SaveChanges();
+                }
+                else
+                { Console.WriteLine("this product not exist"); }
+
+            }
+            
+            //update category 
+            using (var context = new AppDBcontext())
+            {
+                Category category = context.Categories.FirstOrDefault(c => c.Id == 1);
+
+                if (category != null)
+                {
+                    category.Name = "meat";
+                    context.SaveChanges();
+                }
+                else
+                { Console.WriteLine("this category not exist"); }
+
+            }
+            //delete product
+            using (var context = new AppDBcontext())
+            {
+                Product product = context.Products.FirstOrDefault(p => p.Id == 1);
+
+                if (product != null)
+                {
+                    context.Products.Remove(product);
+                    context.SaveChanges();
+                }
+                else
+                { Console.WriteLine("this product not exist"); }
+
+            }
+
+            //delete Categogry
+
+            using (var context = new AppDBcontext())
+            {
+                Category category = context.Categories.FirstOrDefault(c => c.Id == 1);
+
+                if (category != null)
+                {
+                    context.Categories.Remove(category);
+                    context.SaveChanges();
+                }
+                else
+                { Console.WriteLine("this category not exist"); }
+
+            }
+
+            //get product by id 
+            using (var context = new AppDBcontext())
+            {
+                Product product = context.Products.FirstOrDefault(p => p.Id == 1);
+
+                if (product == null)
+                    Console.WriteLine("this product not exist");
+            }
+
+            //get category by id 
+            using (var context = new AppDBcontext())
+            {
+                Category category = context.Categories.FirstOrDefault(c => c.Id == 1);
+
+                if (category == null)
+                    Console.WriteLine("this category not exist");
+            }
+
+
+
         }
     }
 }
